3e58de4eaf2d5d9cb9ae23b0526bf4768177d9af
MeherDeepak-2005 <68017128+MeherDeepak-2005@users.noreply.github.com>
2020-07-08 22:58:54 +0530

    Update assistant.py

--- assistant.py
+++ assistant.py
@@ -52,16 +52,42 @@ def service(authenticate):
         greetings = ["Hey Deepak","Hello, welcome back","Welcome come back Deepak"]
         greet = greetings[random.randint(0,len(greetings)-1)]
         speak(greet) 
-           
+    if if_there(['netflix','movie']):
+        search_term = record_audio("what do yuo want to search")
+        url = f"https://www.netflix.com/search?q={search_term}"
+        webbrowser.get().open(url)
+        speak('enjoy watching')
+        exit()
 
+    if if_there(['youtube']):
+        search_term = record_audio("what do yuo want to watch")
+        url = f"https://www.youtube.com/results?search_query={search_term}"
+        webbrowser.get().open(url)
+        speak('enjoy watching')
+        exit()
+
+    if if_there(['search','google','what is ']):
+        search_term = record_audio('what to search')
+        url = f"https://www.google.com/search?q={search_term}"
+        webbrowser.get().open(url)
+        speak(f"i found this on the web about {search_term}")
+
+    if if_there(['goodbye','bye']):
+        bye = [f"going offline",f"bye","it was nice to meet you",f"have a nice day"]
+        breakpoint_2 = bye[random.randint(0,len(bye)-1)]
+        speak(breakpoint_2)
+        exit()
 
 
 time.sleep(1)
 while(1):
     voice_data = record_audio()
-    if there_exists([""]):
-        print("activated...")
+    if there_exists(["wake up"]):
+        speak("activated...")
         authenticate = record_audio()
         service(authenticate)
+
+    
+
 
     
